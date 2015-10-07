c3cb70b12c3b5cca8427625cea80586503e1bf63
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2015-10-07 09:23:44 +0200

    turned tabs into spaces, what a crime

--- src/index.js
+++ src/index.js
@@ -3,41 +3,41 @@ import $ from 'jquery';
 window.$ = $;
 
 $('#launch').on('mousedown', function(){
-	state.started = true;
+  state.started = true;
 });
 
 $('#right').on('mousedown', function(){
-	state.direction = 'right';
+  state.direction = 'right';
 });
 
 $('#left').on('mousedown', function(){
-	state.direction = 'left';
+  state.direction = 'left';
 });
 
 $('#right, #left').on('mouseleave mouseup', function(){
-	state.direction = '';
+  state.direction = '';
 });
 
 $(document.body).on('keydown', function(e){
-	if(e.keyCode === 39){
-		state.direction = 'right';
-	}else if(e.keyCode === 37){
-		state.direction = 'left';
-	}
+  if(e.keyCode === 39){
+    state.direction = 'right';
+  }else if(e.keyCode === 37){
+    state.direction = 'left';
+  }
 });
 
 $(document.body).on('keyup', function(e){
-	state.direction = '';
+  state.direction = '';
 });
 
 $('#left').on('mousedown', function(){
-	state.left = true;
+  state.left = true;
 });
 
 var state = {
-	started: false,
-	left: false,
-	right: false
+  started: false,
+  left: false,
+  right: false
 };
 
 var height = window.innerHeight - 100;
@@ -82,27 +82,27 @@ stage.addChild(foreground);
 animate();
 
 function animate(i) {
-	i = i || 0;
-	if(i <= 10 && state.started === true){
-		i = i + 0.03;
-	}
-	setTimeout(animate.bind(this, i), 20);
+  i = i || 0;
+  if(i <= 10 && state.started === true){
+    i = i + 0.03;
+  }
+  setTimeout(animate.bind(this, i), 20);
 
-	// just for fun, let's rotate mr rabbit a little
-	if(rocket.position.y >= (height*0.8)){
-		rocket.position.y -= i;
-	}else{
-		clouds.tilePosition.y += i;
-	}
+  // just for fun, let's rotate mr rabbit a little
+  if(rocket.position.y >= (height*0.8)){
+    rocket.position.y -= i;
+  }else{
+    clouds.tilePosition.y += i;
+  }
 
-	if(state.direction === 'right'){
-		clouds.tilePosition.x -= 8;
-	}else if(state.direction === 'left'){
-		clouds.tilePosition.x += 8;
-	}
+  if(state.direction === 'right'){
+    clouds.tilePosition.x -= 8;
+  }else if(state.direction === 'left'){
+    clouds.tilePosition.x += 8;
+  }
 
 
-	// render the container
-	renderer.render(stage);
+  // render the container
+  renderer.render(stage);
 }
 
