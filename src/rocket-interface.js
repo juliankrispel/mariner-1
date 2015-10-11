5c63b663faf8ffdadfc06b3bca6f19ab1000572f
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2015-10-12 01:08:26 +0200

    bare rocket-interface function

--- src/rocket-interface.js
+++ src/rocket-interface.js
@@ -1,25 +1,5 @@
 import $ from 'jquery';
 import actions from './rocket-actions';
 
-var state = {
-  hasLaunched: false
-}
-
-export {state};
-
 export function renderRocketInterface(){
-  // The template is just a string
-  var template = `
-  <div class="rocket-interface">
-  </div>
-  `;
-
-  var el = $(template);
-
-  //  $('.launch-button', el).click(function(){
-  //    state.hasLaunched = true;
-  //    actions.launchRocket();
-  //  });
-
-  return el;
 }
