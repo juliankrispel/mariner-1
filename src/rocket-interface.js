80ce95b9b71862df9cc9e43d18b370f79b8ef639
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2015-10-11 12:02:56 +0200

    include rocket actions

--- src/rocket-interface.js
+++ src/rocket-interface.js
@@ -4,13 +4,17 @@ import {
   steerRight, 
   stopSteering, 
   launchRocket  
-} from './rocket';
+} from './rocket-actions';
 
 export function renderRocketInterface(container){
+
+	// The template is just a string
   var template = `
     <button class="launch-button">Launch Rocket</button>
   `;
+
   var el = $(container).append($(template));
+
   $('.launch-button', el).click(function(){
     launchRocket();
   });
