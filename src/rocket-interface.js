32b67e85b430afb8a883a9824b852f15b60b3a43
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2015-10-11 10:56:12 +0200

    more stuff

--- src/rocket-interface.js
+++ src/rocket-interface.js
@@ -1,2 +1,19 @@
 import $ from 'jquery';
-import { steerLeft, steerRight, stopSteering, launchRocket  } from './rocket';
+import { 
+  steerLeft, 
+  steerRight, 
+  stopSteering, 
+  launchRocket  
+} from './rocket';
+
+function renderRocketInterface(container){
+  var template = `
+    <button class="launch-button">Launch Rocket</button>
+  `;
+  var el = $(container).append($(template));
+  $('.launch-button', el).click(function(){
+    launchRocket();
+  });
+}
+
+renderRocketInterface('#interface');
