a11856c45e3542864d93e1ae4b13a0e7bf336e55
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2015-10-11 18:46:53 +0200

    start from scratch

--- src/rocket-interface.js
+++ src/rocket-interface.js
@@ -7,61 +7,19 @@ var state = {
 
 export {state};
 
-export function renderLaunchButton(){
-  var template = '<button class="launch-button">Launch Rocket</button>';
-  var el = $(template);
-  return el;
-}
-
-export function renderRightButton(){
-  var template = '<button class="steer-right float-right">></button>';
-  var el = $(template);
-  state.hasLaunched = false;
-
-  $(el).on('mouseup mouseleave', function(){
-    actions.stopSteering();
-  });
-
-  $(el).on('mousedown', function(){
-    if(state.hasLaunched === true){
-      actions.steerRight();
-    }
-  });
-
-  return el;
-}
-
-export function renderLeftButton(){
-  var template = '<button class="steer-left float-right"><</button>';
-  var el = $(template);
-
-  $(el).on('mouseup mouseleave', function(){
-    actions.stopSteering();
-  });
-
-  $(el).on('mousedown', function(){
-    if(state.hasLaunched === true){
-      actions.steerLeft();
-    }
-  });
-
-  return el;
-}
-
 export function renderRocketInterface(){
   // The template is just a string
-  var template = `<div class="rocket-interface"> </div>`;
+  var template = `
+  <div class="rocket-interface">
+  </div>
+  `;
 
   var el = $(template);
 
-  el.append(renderLaunchButton());
-  el.append(renderRightButton());
-  el.append(renderLeftButton());
-
-  $('.launch-button', el).click(function(){
-    state.hasLaunched = true;
-    actions.launchRocket();
-  });
+  //  $('.launch-button', el).click(function(){
+  //    state.hasLaunched = true;
+  //    actions.launchRocket();
+  //  });
 
   return el;
 }
--- test/rocket-interface-test.js
+++ test/rocket-interface-test.js
@@ -9,35 +9,5 @@ describe('Rocket Interface', function(){
   });
 
   it('should render a launch button', function(){
-    expect($('button.launch-button').length).toBe(1);
-    expect($('button.steer-right').length).toBe(1);
-    expect($('button.steer-left').length).toBe(1);
-  });
-
-  it('should launch a rocket when the launch button is clicked', function(){
-    let spy = spyOn(actions, 'launchRocket');
-    $('.launch-button').click();
-    expect(actions.launchRocket).toHaveBeenCalled();
-    spy.restore();
-  });
-
-  it('hasLaunched should return true if the rocket has launched', function(){
-    $('.launch-button').click();
-    expect(state.hasLaunched).toBe(true);
-  });
-
-  it('should not steer right if it hasn\'t launched yet', function(){
-    let spy = spyOn(actions, 'steerRight');
-    $('.steer-right').trigger('mousedown');
-    expect(spy).toNotHaveBeenCalled()
-    spy.restore();
-  });
-
-  it('should steer right if it has launched', function(){
-    let spy = spyOn(actions, 'steerRight');
-    $('.launch-button').click()
-    $('.steer-right').trigger('mousedown');
-    expect(spy).toHaveBeenCalled()
-    spy.restore();
   });
 });
