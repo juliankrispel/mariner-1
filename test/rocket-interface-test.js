76f61354643d32cf06b5615306222039133cffc3
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2015-10-12 01:08:59 +0200

    bare test

--- test/rocket-interface-test.js
+++ test/rocket-interface-test.js
@@ -3,11 +3,4 @@ import actions from '../src/rocket-actions';
 import {state, renderRocketInterface} from '../src/rocket-interface';
 
 describe('Rocket Interface', function(){
-  beforeEach(function(){
-    $('body').append(renderRocketInterface());
-    state.hasLaunched = false;
-  });
-
-  it('should render a launch button', function(){
-  });
 });
