a9eb63634dddcf0e4163010cb9299a5190e42683
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2015-10-12 02:59:05 +0200

    start from scratch

--- src/rocket-interface.js
+++ src/rocket-interface.js
@@ -1,35 +1,5 @@
 import $ from 'jquery';
 import actions from './rocket-actions';
 
-export function renderLaunchButton(){
-  let template = '<button class="launch-rocket">Launch</button>';
-  var el = $(template);
-
-  $(el).click(function(){
-    actions.launchRocket();
-  });
-
-  return el;
-}
-
 export function renderRocketInterface(){
-  var template = `
-  <div class="rocket-interface">
-  <button class="steer-right float-right">></button>
-  <button class="steer-left float-right"><</button>
-  </div>
-  `;
-
-  var el = $(template);
-  el.append(renderLaunchButton());
-
-  $('.steer-right', el).click(function(){
-    actions.steerRight();
-  });
-
-  $('.steer-left', el).click(function(){
-    actions.steerLeft();
-  });
-
-  return el;
 }
