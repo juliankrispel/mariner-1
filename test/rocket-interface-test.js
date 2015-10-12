b5c775b492732764e70f014e188bfa13624a483a
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2015-10-12 03:31:32 +0200

    finished everything

--- test/rocket-interface-test.js
+++ test/rocket-interface-test.js
@@ -2,5 +2,26 @@ import $ from 'jquery';
 import actions from '../src/rocket-actions';
 import {renderRocketInterface} from '../src/rocket-interface';
 
-describe('Rocket Interface', function(){
+it('should launch the rocket if the button is pressed', function(){
+  let el = renderRocketInterface();
+  let spy = spyOn(actions, 'launchRocket');
+
+  $('.launch-rocket', el).click();
+  expect(spy).toHaveBeenCalled();
+});
+
+it('should steer right if the right button is pressed', function(){
+  let el = renderRocketInterface();
+  let spy = spyOn(actions, 'steerRight');
+
+  $('.steer-right', el).click();
+  expect(spy).toHaveBeenCalled();
+});
+
+it('should steer left if the left button is pressed', function(){
+  let el = renderRocketInterface();
+  let spy = spyOn(actions, 'steerLeft');
+
+  $('.steer-left', el).click();
+  expect(spy).toHaveBeenCalled();
 });
