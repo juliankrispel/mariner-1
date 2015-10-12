1c702cae092c7ee5386e15b97c941400a126348e
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2015-10-12 02:51:11 +0200

    finished the thing

--- test/rocket-interface-test.js
+++ test/rocket-interface-test.js
@@ -3,4 +3,31 @@ import actions from '../src/rocket-actions';
 import {renderRocketInterface} from '../src/rocket-interface';
 
 describe('Rocket Interface', function(){
+  describe('Rocket Launch Button', function(){
+    it('should launch a rocket when clicked', function(){
+      let spy = spyOn(actions, 'launchRocket');
+      let el = renderRocketInterface();
+      el.find('button.launch-rocket').click();
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+
+  describe('Steer Right Button', function(){
+    it('should steer the rocket right', function(){
+      let spy = spyOn(actions, 'steerRight');
+      let el = renderRocketInterface();
+      el.find('button.steer-right').click();
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+
+  describe('Steer Left Button', function(){
+    it('should steer the rocket left', function(){
+      let spy = spyOn(actions, 'steerLeft');
+      let el = renderRocketInterface();
+      el.find('button.steer-left').click();
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+
 });
