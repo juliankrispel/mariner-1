2a98fa4adf939e57346128592e94e620632ea831
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2015-10-12 16:45:37 +0200

    fixed julians shit

--- src/rocket-interface.js
+++ src/rocket-interface.js
@@ -1,28 +1,35 @@
 import $ from 'jquery';
 import rocketControl from './rocket-control';
 
-export function renderRocketInterface(){
-  // The template is just a string
-  var template = `
-  <div class="rocket-interface">
-  <button class="launch-rocket">Launch</button>
-  <button class="steer-right float-right">></button>
-  <button class="steer-left float-right"><</button>
-  </div>
-  `;
-
+export function renderLaunchButton(){
+  var template = '<button class="launch-rocket">Launch</button>';
   var el = $(template);
 
   $('.launch-rocket', el).click(function(){
     rocketControl.launchRocket();
   });
 
-  $('.steer-righht', el).click(function(){
-    rocketControl.steerRight;
+  return el;
+}
+
+export function renderRocketInterface(){
+  // The template is just a string
+  var template = `
+  <div class="rocket-interface">
+  <button class="steer-right float-right">></button>
+  <button class="steer-left float-right"><</button>
+  </div>
+  `;
+
+  var el = $(template);
+  el.prepend(renderLaunchButton());
+
+  $('.steer-right', el).click(function(){
+    rocketControl.steerRight();
   });
 
   $('.steer-left', el).click(function(){
-    rocketControl.steerRight();
+    rocketControl.steerLeft();
   });
 
   return el;
--- test/rocket-interface-test.js
+++ test/rocket-interface-test.js
@@ -1,3 +1,24 @@
 import $ from 'jquery';
 import rocketControl from '../src/rocket-control';
 import {renderRocketInterface} from '../src/rocket-interface';
+
+it('should launch the rocket when the launch button is pressed', function(){
+  var spy = spyOn(rocketControl, 'launchRocket');
+  var el = renderRocketInterface();
+  el.find('.launch-rocket').click();
+  expect(spy).toHaveBeenCalled();
+});
+
+it('should steer the rocket to the right when the steer-right button is pressed', function(){
+  var spy = spyOn(rocketControl, 'steerRight');
+  var el = renderRocketInterface();
+  el.find('.steer-right').click();
+  expect(spy).toHaveBeenCalled();
+});
+
+it('should steer the rocket to the left when the steer-left button is pressed', function(){
+  var spy = spyOn(rocketControl, 'steerLeft');
+  var el = renderRocketInterface();
+  el.find('.steer-left').click();
+  expect(spy).toHaveBeenCalled();
+});
