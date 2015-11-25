9688c6a4e5ee5208a7a1c373a096bbd7f12801c0
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2015-11-25 21:20:36 +0100

    more modular hahaha

--- src/rocket-interface.js
+++ src/rocket-interface.js
@@ -1,29 +1,46 @@
 import $ from 'jquery';
 import rocketControl from './rocket-control';
 
+function renderLaunchButton() {
+  var template = '<button class="launch-rocket">Launch</button>';
+  var el = $(template);
+  $(el).click(function(){
+    rocketControl.launchRocket();
+  });
+  return el;
+}
+
+function renderRightButton() {
+  var template = '<button class="steer-right float-right">></button>';
+  var el = $(template);
+
+  $(el).click(function(){
+    rocketControl.steerRight();
+  });
+  return el;
+
+}
+
+function renderLeftButton() {
+  var template = '<button class="steer-left float-right"><</button>';
+  var el = $(template);
+  $('.steer-left', el).click(function(){
+    rocketControl.steerLeft();
+  });
+  return el;
+}
+
 export function renderRocketInterface(){
   // The template is just a string
   var template = `
   <div class="rocket-interface">
-  <button class="launch-rocket">Launch</button>
-  <button class="steer-right float-right">></button>
-  <button class="steer-left float-right"><</button>
   </div>
   `;
 
   var el = $(template);
-
-  $('.launch-rocket', el).click(function(){
-    rocketControl.launchRocket();
-  });
-
-  $('.steer-righht', el).click(function(){
-    rocketControl.steerRight;
-  });
-
-  $('.steer-left', el).click(function(){
-    rocketControl.steerRight();
-  });
-
+  el.append(renderLaunchButton());
+  el.append(renderRightButton());
+  el.append(renderLeftButton());
+  
   return el;
 }
--- test/rocket-interface-test.js
+++ test/rocket-interface-test.js
@@ -1,3 +1,27 @@
 import $ from 'jquery';
 import rocketControl from '../src/rocket-control';
 import {renderRocketInterface} from '../src/rocket-interface';
+
+describe('rocketInterface', ()=> {
+  beforeEach(()=> {
+    $('body').append(renderRocketInterface());
+  });
+
+  it('launches the rocket when the launch button is pressed', ()=> {
+    spyOn(rocketControl, 'launchRocket');
+    $('.launch-rocket').click();
+    expect(rocketControl.launchRocket).toHaveBeenCalled();
+  });
+
+  it('steers right when the right button is pressed', ()=> {
+    spyOn(rocketControl, 'steerRight');
+    $('.steer-right').click();
+    expect(rocketControl.steerRight).toHaveBeenCalled();
+  });
+
+  it('steers left when the left button is pressed', ()=> {
+    spyOn(rocketControl, 'steerLeft');
+    $('.steer-left').click();
+    expect(rocketControl.steerLeft).toHaveBeenCalled();
+  });
+});
