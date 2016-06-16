9880eafedfac12d5cba3607dd6d5fc948182bb51
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2016-06-16 09:29:33 +0200

    fixed julians crap

--- src/rocket-interface.js
+++ src/rocket-interface.js
@@ -1,29 +1,57 @@
 import $ from 'jquery';
 import rocketControl from './rocket-control';
 
+export function renderRightButton() {
+  var template = `
+  <button class="steer-right float-right">></button>
+  `;
+  var el = $(template);
+
+  $(el).click(function(){
+    rocketControl.steerRight();
+  });
+
+  return el;
+}
+
+export function renderLaunchButton() {
+  var template = `
+  <button class="launch-rocket">Launch</button>
+  `;
+  var el = $(template);
+
+  $(el).click(function(){
+    rocketControl.launchRocket();
+  });
+
+  return el;
+}
+
+export function renderLeftButton() {
+  var template = `
+  <button class="steer-left float-right"><</button>
+  `;
+  var el = $(template);
+
+  $(el).click(function(){
+    rocketControl.steerLeft();
+  });
+
+  return el;
+}
+
 export function renderRocketInterface(){
   // The template is just a string
   var template = `
   <div class="rocket-interface">
-  <button class="launch-rocket">Launch</button>
-  <button class="steer-right float-right">></button>
-  <button class="steer-left float-right"><</button>
   </div>
   `;
 
   var el = $(template);
 
-  $('.launch-rocket', el).click(function(){
-    rocketControl.launchRocket();
-  });
-
-  $('.steer-right', el).click(function(){
-    rocketControl.steerRight();
-  });
-
-  $('.steer-left', el).click(function(){
-    rocketControl.steerLeft();
-  });
+  el.append(renderRightButton());
+  el.append(renderLeftButton());
+  el.append(renderLaunchButton());
 
   return el;
 }
